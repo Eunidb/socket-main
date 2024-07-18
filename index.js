a2b53a918332858a45b378b2bae7de1cb92c83c3
Eunidb <a3521110133@alumno.uttehuacan.edu.mx>
2024-07-18 17:02:43 -0600

    Cambio 2

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express")
 const {createServer} = require("http")
 const {Server} = require("socket.io")
 
+const httpServer = createServer();
 
 const io = new Server(httpServer, {
     cors: {
